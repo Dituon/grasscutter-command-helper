e140f85bf3c934f898db953c03801e99d3dd0b9f
Dituon <68615161+Dituon@users.noreply.github.com>
2023-06-10 23:29:46 +0900

    fix url param loader

--- src/module/app/app.js
+++ src/module/app/app.js
@@ -4,3 +4,6 @@ import '../share'
 
 import './app.css'
 import '../utils/ads.js'
+
+import { loadUrlParam } from './url-param'
+loadUrlParam()
--- src/module/app/url-param.js
+++ src/module/app/url-param.js
@@ -2,32 +2,35 @@ import { commandVersionSelectElement, initCommand } from "../command/command-loa
 import { menu } from "../command/command-menu.js"
 import { config, importedList } from "./init.js"
 import { initLang, langData } from "./lang-loader.js"
-import { server, setServer } from "../remote/remote-execute.js"
+import { setServer } from "../remote/remote-execute.js"
 import { showMessage } from "../ui/ui.js"
 
 export const urlParams = new URL(location.href).searchParams
-urlParams.forEach((value, key) => {
-    switch (key) {
-        case 'server':
-            setServer(decodeURIComponent(value))
-            break
-        case 'import':
-            if (parseInt(value)) value = `${window.location.origin}/share/${value}.gmh`
-            console.debug(value)
-            if (importedList.includes(value)) break
-            fetch(decodeURIComponent(value)).then(p => p.text())
-                .then(raw => menu.importCommand(raw))
-                .catch(e => showMessage(langData.commandImportFail, 3000))
-                .finally(() => importedList.push(value))
-            break
-        case 'lang':
-            initLang(value)
-            initCommand(config.commandVersion)
-            break
-        case 'version':
-            if (!['1.2.1', '1.4.2', '1.6.1', 'GM'].includes(value)) break
-            commandVersionSelectElement.value = value
-            initCommand(value)
-            break
-    }
-})
+
+export function loadUrlParam(){
+    urlParams.forEach((value, key) => {
+        switch (key) {
+            case 'server':
+                setServer(decodeURIComponent(value))
+                break
+            case 'import':
+                if (parseInt(value)) value = `${window.location.origin}/share/${value}.gmh`
+                console.debug(value)
+                if (importedList.includes(value)) break
+                fetch(decodeURIComponent(value)).then(p => p.text())
+                    .then(raw => menu.importCommand(raw))
+                    .catch(e => showMessage(langData.commandImportFail, 3000))
+                    .finally(() => importedList.push(value))
+                break
+            case 'lang':
+                initLang(value)
+                initCommand(config.commandVersion)
+                break
+            case 'version':
+                if (!['1.2.1', '1.4.2', '1.6.1', 'GM'].includes(value)) break
+                commandVersionSelectElement.value = value
+                initCommand(value)
+                break
+        }
+    })
+}
